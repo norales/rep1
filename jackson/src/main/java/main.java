9ea80ea659542d4dfc0862817ae8cb4945d351e0
norales <64035236+norales@users.noreply.github.com>
2022-07-25 14:25:31 +0200

    Add files via upload

---
+++ jackson/src/main/java/main.java
@@ -0,0 +1,12 @@
+public class main {
+    private String a;
+    private int e;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+}
